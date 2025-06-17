a6a458dd51de1ce3adcd8d0d2dfab8adb26659b2
doccaico <hash_ext_luna@yahoo.co.jp>
2025-06-18 00:25:29 +0900

    up

---
+++ raylib/raylib_ext.c.v
@@ -0,0 +1,207 @@
+// These are manually-made bindings for parts of the library that do not need
+// or validate custom handling for.
+// Functions that use strings get bindings here so that I can allow them to use
+// V-strings as arguments/return types instead of C-strings
+
+module raylib
+
+$if windows {
+	// #preinclude "@VMODROOT/src/c/pre.h"
+
+	// #flag @VMODROOT/thirdparty/raylib/src/rcore.o
+
+	// #flag -DPLATFORM_DESKTOP
+
+	#flag -I @VMODROOT/raylib
+	#flag -I @VMODROOT/raymath
+	#flag -I @VMODROOT/raygui
+	#flag -I @VMODROOT/rlgl
+
+	// #flag -I @VMODROOT/thirdparty/raylib/src/
+	// #flag -I@VMODROOT/thirdparty/raylib/src/
+	// #flag -L@VMODROOT/thirdparty/raylib/src/
+	// #flag -L@VMODROOT/
+	#flag -L @VMODROOT/libs/windows
+
+	// #flag -fno-lto
+
+	// #flag -L.
+	// #flag -I .
+
+	// #include "raylib.h"
+	// -no-prod-options -cflags "-O2"
+	// -lopengl32 -luser32 -lopengl32
+	#flag -DNOUSER -DNOSHOWWINDOW -DNOGDI -D_NDEBUG -DNDEBUG
+	#flag -lraylib@START_LIBS
+	#flag -lgdi32 -lwinmm
+
+	$if prod && !debug {
+		#flag -mwindows
+	}
+} $else {
+	$compile_error('Unsupported OS')
+}
+
+#include <raylib.h>
+
+@[typedef]
+pub struct C.va_list {}
+
+pub struct C.rAudioBuffer {}
+
+pub struct C.rAudioProcessor {}
+
+pub type AudioBuffer = C.rAudioBuffer
+pub type AudioProcessor = C.rAudioProcessor
+
+pub const lightgray = Color{200, 200, 200, 255}
+pub const gray = Color{130, 130, 130, 255}
+pub const darkgray = Color{80, 80, 80, 255}
+pub const yellow = Color{253, 249, 0, 255}
+pub const gold = Color{255, 203, 0, 255}
+pub const orange = Color{255, 161, 0, 255}
+pub const pink = Color{255, 109, 194, 255}
+pub const red = Color{230, 41, 55, 255}
+pub const maroon = Color{190, 33, 55, 255}
+pub const green = Color{0, 228, 48, 255}
+pub const lime = Color{0, 158, 47, 255}
+pub const darkgreen = Color{0, 117, 44, 255}
+pub const skyblue = Color{102, 191, 255, 255}
+pub const blue = Color{0, 121, 241, 255}
+pub const darkblue = Color{0, 82, 172, 255}
+pub const purple = Color{200, 122, 255, 255}
+pub const violet = Color{135, 60, 190, 255}
+pub const darkpurple = Color{112, 31, 126, 255}
+pub const beige = Color{211, 176, 131, 255}
+pub const brown = Color{127, 106, 79, 255}
+pub const darkbrown = Color{76, 63, 47, 255}
+
+pub const white = Color{255, 255, 255, 255}
+pub const black = Color{0, 0, 0, 255}
+pub const blank = Color{0, 0, 0, 0}
+pub const magenta = Color{255, 0, 255, 255}
+pub const raywhite = Color{245, 245, 245, 255}
+
+pub const camera_perspective = 0
+pub const camera_orthographic = 1
+
+pub const camera_free = 1
+pub const camera_orbital = 2
+pub const camera_first_person = 3
+pub const camera_third_person = 4
+
+// Keyboard related
+
+// vis_key_pressed allows you to pass a `KeyboardKey` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_key_pressed(key KeyboardKey) bool {
+	return is_key_pressed(int(key))
+}
+
+// vis_key_pressed_repeat allows you to pass a `KeyboardKey` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_key_pressed_repeat(key KeyboardKey) bool {
+	return is_key_pressed_repeat(int(key))
+}
+
+// vis_key_down allows you to pass a `KeyboardKey` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_key_down(key KeyboardKey) bool {
+	return is_key_down(int(key))
+}
+
+// vis_key_up allows you to pass a `KeyboardKey` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_key_up(key KeyboardKey) bool {
+	return is_key_up(int(key))
+}
+
+// vis_key_released allows you to pass a `KeyboardKey` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_key_released(key KeyboardKey) bool {
+	return is_key_released(int(key))
+}
+
+// vset_exit_key allows you to pass a `KeyboardKey` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vset_exit_key(key KeyboardKey) {
+	set_exit_key(int(key))
+}
+
+// log level
+
+// vset_trace_log_level allows you to pass a `TraceLogLevel` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vset_trace_log_level(log_level TraceLogLevel) {
+	set_trace_log_level(int(log_level))
+}
+
+// Mouse button related
+
+// vis_mouse_button_pressed allows you to pass a `MouseButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_mouse_button_pressed(button MouseButton) bool {
+	return is_mouse_button_pressed(int(button))
+}
+
+// vis_mouse_button_down allows you to pass a `MouseButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_mouse_button_down(button MouseButton) bool {
+	return is_mouse_button_down(int(button))
+}
+
+// vis_mouse_button_released allows you to pass a `MouseButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_mouse_button_released(button MouseButton) bool {
+	return is_mouse_button_released(int(button))
+}
+
+// vis_mouse_button_up allows you to pass a `MouseButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_mouse_button_up(button MouseButton) bool {
+	return is_mouse_button_up(int(button))
+}
+
+// GamePad related
+
+//  vis_gamepad_aviable allows you to pass a `GamepadButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_gamepad_aviable(gamepad GamepadButton) bool {
+	return is_gamepad_available(int(gamepad))
+}
+
+//  vis_gamepad_button_pressed allows you to pass a `GamepadButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_gamepad_button_pressed(gamepad int, button GamepadButton) bool {
+	return is_gamepad_button_pressed(gamepad, int(button))
+}
+
+//  vis_gamepad_button_down allows you to pass a `GamepadButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_gamepad_button_down(gamepad int, button GamepadButton) bool {
+	return is_gamepad_button_down(gamepad, int(button))
+}
+
+//  vis_gamepad_button_released allows you to pass a `GamepadButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_gamepad_button_released(gamepad int, button GamepadButton) bool {
+	return is_gamepad_button_released(gamepad, int(button))
+}
+
+//  vis_gamepad_button_up allows you to pass a `GamepadButton` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_gamepad_button_up(gamepad int, button GamepadButton) bool {
+	return is_gamepad_button_up(gamepad, int(button))
+}
+
+//  vget_gamepad_axis_movement allows you to pass a `GamepadAxis` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vget_gamepad_axis_movement(gamepad int, axis GamepadAxis) f32 {
+	return get_gamepad_axis_movement(gamepad, int(axis))
+}
+
+//  vis_gesture_detected allows you to pass a `Gesture` enum value in your V programs, instead of a plain int.
+@[inline]
+pub fn vis_gesture_detected(gesture Gesture) bool {
+	return is_gesture_detected(u32(gesture))
+}
---
+++ raylib/raylib_test.v
@@ -0,0 +1,18 @@
+module main
+
+import raylib
+
+fn test_raylib() {
+	raylib.set_config_flags(raylib.ConfigFlags.flag_window_resizable)
+	raylib.init_window(800, 600, 'Hello, World!')
+	raylib.set_target_fps(30)
+
+	for !raylib.window_should_close() {
+		raylib.begin_drawing()
+		raylib.clear_background(raylib.white)
+		raylib.draw_fps(raylib.get_screen_width() / 2, raylib.get_screen_height() / 2)
+		raylib.end_drawing()
+	}
+
+	raylib.close_window()
+}
